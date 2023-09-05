0cddba00a8f0027840a5ef90da3a70564b3e54af
CarlosCoder-MX <carloscoder.contact@gmail.com>
2023-09-05 12:01:21 -0600

    Added navigation to questionsScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz_app/start_screen.dart';
 import 'package:flutter_quiz_app/questions_screen.dart';
+import 'package:flutter_quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
-const Quiz({super.key});
+  const Quiz({super.key});
 
- @override
+  @override
   State<Quiz> createState() {
     return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen();
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
+  // Widget? activeScreen;
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  // void switchScreen() {
+  //   setState(() {
+  //     activeScreen = const QuestionsScreen();
+  //   });
+  // }
 
   @override
   Widget build(context) {
-    return 
-
-    MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
@@ -38,7 +49,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
